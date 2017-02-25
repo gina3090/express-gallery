30cace95a9c019f81b9b809567b87c0104c22624
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-02-25 01:29:33 -1000

    updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const app = express();
+
+const CONFIG = require('./config/config.json');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+
+const db = require('./models');
+const Users = db.Users;
+
+const user = require('./routes/user');
 const gallery = require('./routes/gallery');
 
 const hbs = handlebars.create({
@@ -13,15 +25,60 @@ const hbs = handlebars.create({
 app.engine('.hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-app.use(express.static('public'));
+app.use(cookieParser());
 
+app.use(session({
+  store: new RedisStore(),
+  secret: CONFIG.SESSION_SECRET
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Users.findOne({
+      where: {
+        username: username,
+        password: password
+      }
+    })
+    .then((user) => {
+      if(!user) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  return done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  return done(null, user);
+});
+
+app.use('/login', user);
 app.use('/gallery', gallery);
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/login', (req, res) => {
+  res.render('partials/login');
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/gallery',
+  failureRedirect: '/login'
+}));
 
 module.exports = app;
